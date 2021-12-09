1d4f1da1b76bd3aeda661ec938033cb6969184cb
iqking613 <17740875@qq.com>
2021-12-09 12:28:13 +0800

    test

--- scipy1.py
+++ scipy1.py
@@ -1,15 +1,15 @@
 import numpy as np
 from scipy.cluster.vq import vq,kmeans,whiten
 
-# list1=[88.0,74.0,96.0,85.0]
-# list2=[92.0,99.0,96.0,95.0]
-# list3=[91.0,87.0,96.0,95.0]
-# list4=[78.0,99.0,96.0,81.0]
-# list5=[88.0,78.0,98.0,84.0]
-# list6=[100.0,95.0,100.0,92.0]
-data = np.array([list1,list2,list3,list4,list5,list6])
+list1=[88.0,74.0,96.0,85.0]
+list2=[92.0,99.0,96.0,95.0]
+list3=[91.0,87.0,96.0,95.0]
+list4=[78.0,99.0,96.0,81.0]
+list5=[88.0,78.0,98.0,84.0]
+list6=[100.0,95.0,100.0,92.0]
+list7 = [100.0,99.0,92.0,100.0]
+data = np.array([list1,list2,list3,list4,list5,list6],list7)
 whiten = whiten(data)
 centroids,_=kmeans(whiten,2)
 result,_=vq(whiten,centroids)
-print(result)
 print(result)
